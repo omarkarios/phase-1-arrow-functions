183485939ab42dd503f8c7bdf59c3a9f49cab09c
omarkarios <markariosoketch@gmail.com>
2025-09-14 21:22:51 +0300

    Completed lab.

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(num1,num2){
+    return num1 / num2;
+}
+console.log(divide(2000,100))
+
+const square = num3 => num3**2;
+console.log(square(2))
+
+const add = (num4, num5)=>(num4 + num5);
+console.log(add(1,5));
